1da3e25078e10ef2e9c5c8a3bfda09802e666ba6
Ijiwole <ijiwoleadedamola2001@gmail.com>
2024-05-21 16:12:18 +0100

    initial commit

--- middleware/protect_user.js
+++ middleware/protect_user.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
 const UserModel = require("../models/user");
 const jwt = require("jsonwebtoken");
-const CustomError = require("../errors");
+const {StatusCodes} = require('http-status-codes')
 
 const protect_user = async (req, res, next) => {
   let token;
   const auth = req.headers.authorization;
   if (!auth || !auth.startsWith("Bearer ")) {
-    throw new CustomError.BadRequestError("Invalid authorization format");
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      message: "Invalid authorization format"
+    })
   }
 
   token = auth.split(" ")[1];
